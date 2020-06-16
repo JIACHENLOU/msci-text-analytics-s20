10c3c39cffe53b5cdc4f6edf7930984ff1c69aae
JIACHENLOU <65255591+JIACHENLOU@users.noreply.github.com>
2020-06-15 21:45:22 -0400

    Delete inference.py

--- inference.py
+++
@@ -1,6 +0,0 @@
-def prediction(X_test, clf, count_vect, tfidf_transformer):
-    X_test_count = count_vect.transform(X_test)
-    X_test_tfidf = tfidf_transformer.transform(X_test_count)
-    predict = clf.predict(X_test_tfidf)
-    return predict
-
